a51c10147ed07ae6d4ed92d9474c21e15e797692
estrella-kim <puris9030@gmail.com>
2018-01-24 15:57:28 +0900

    mysql 연동

--- renewal/app.js
+++ renewal/app.js
@@ -10,6 +10,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 var login = require('./routes/login');
 
+
 var app = express();
 
 // view engine setup
--- renewal/routes/index.js
+++ renewal/routes/index.js
@@ -1,7 +1,21 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
 var router = express.Router();
+var mysql = require('mysql');
 
+var connection = mysql.createConnection({
+    host : '127.0.0.1',
+    port : '3306',
+    user : 'me',
+    password : 'secret'
+});
+connection.connect();
+
+connection.query('SELECT 1+1 AS solution', function(err, results, fields) {
+    if(err) throw err;
+    console.log('The solution is :', results[0].solution);
+})
+connection.end();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send({ title: 'Express' });
